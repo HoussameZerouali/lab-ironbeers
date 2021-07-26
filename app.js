4d0639a88a6524d610627ab6bf2989341ce9b003
Houssame Zerouali <azenax328@gmail.com>
2021-07-26 15:58:35 +0200

    lab solved without bonuses

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Register the location for handlebars partials here:
 
@@ -19,7 +19,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    css: ["index.css"]
+  });
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then((response) => {
+    res.render('beers.hbs', {
+      beers: response,
+      css: ["beersPage.css"],
+    })
+  })
+  
+})
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom().
+  then((response) => {
+    res.render('random-beer.hbs', {
+      beer: response[0],
+      css: ["beersPage.css"]
+    })
+  })
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
